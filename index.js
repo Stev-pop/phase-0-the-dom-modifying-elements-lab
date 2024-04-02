b4c00720e9eb9e2ef9d103dbcacf364be850529d
Stephen Onyango <onyangosteve01@gmail.com>
2024-04-02 13:49:37 +0300

    creates h1 element

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = "victory"
+const yourName = 'Stphen Onyango' 
+newHeader.textContent = `${yourName} is the champion`;
+newHeader.className = "victory";
+document.body.append(newHeader);
